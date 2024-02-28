993ae8686cee42d035f782e8351293e816db3491
parkerJenkins36 <pjenki17@uncc.edu>
2024-02-27 17:28:55 -0800

    ad

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -1,7 +1,7 @@
 function loadImage() {
     var image = document.getElementById('introImage').files[0]; 
     const imageUrl = URL.createObjectURL(image); 
-    var text = "<img src=\"" + imageUrl + "\"+>";
+    var text = "<img src=\"" + imageUrl + "\" >";
     document.getElementById('loadImage').innerHTML = text;
 }
 
