e1201d6919e64276f696e9a0b6fd252f77874b9b
parkerJenkins36 <pjenki17@uncc.edu>
2024-04-13 06:17:55 -0700

    a

--- clientproject/scripts/client.js
+++ clientproject/scripts/client.js
@@ -9,6 +9,8 @@ document.addEventListener("click", function(event) {
         dropdown.style.display = "none";
     }
 });
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,6 +28,7 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -36,4 +39,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
