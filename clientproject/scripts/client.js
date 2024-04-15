5eb1ac092cb82c3f1b82137ff4ed05c6482c03ee
parkerJenkins36 <pjenki17@uncc.edu>
2024-04-15 01:04:41 -0700

    add

--- clientproject/scripts/client.js
+++ clientproject/scripts/client.js
@@ -1,10 +1,10 @@
 function toggleDropdown() {
-    var dropdown = document.getElementById("dropdown");
+    let dropdown = document.getElementById("dropdown");
     dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 }
 document.addEventListener("click", function(event) {
-    var dropdown = document.getElementById("dropdown");
-    var button = document.querySelector(".more-details-button");
+    let dropdown = document.getElementById("dropdown");
+    let button = document.querySelector(".more-details-button");
     if (!button.contains(event.target) && !dropdown.contains(event.target)) {
         dropdown.style.display = "none";
     }
